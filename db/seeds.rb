dd233de009a6e8eff47f3ea51e8f3dffdcf51fc9
Ovsjah <ovsjah@gmail.com>
2020-03-01 16:47:40 +0200

    Complete Odin Flight Booker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airports = %w[
+  ATL PEK LAX HND DXB
+  ORD LHR PVG HKG CDG
+  DFW CAN ICN AMS FRA
+  SIN BKK DEN DEL CGK
+  JFK KUL MAD SFO CTU
+  SZX MCO LAS BCN YYZ
+]
+
+Airport.insert_all(airports.map { |abbr| {code: abbr, created_at: Time.now, updated_at: Time.now} })
+
+Flight.insert_all(
+  Airport.ids.each_slice(2).map do |ids|
+    {departure_id: ids.first,
+     arrival_id: ids.last,
+     date: rand(13.days).seconds.from_now,
+     duration: rand(24.hours).to_i,
+     created_at: Time.now,
+     updated_at: Time.now}
+  end
+)
