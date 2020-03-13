7ff28bff6aebe093c43384605b477c7bf633beb9
Ovsjah <ovsjah@gmail.com>
2020-03-13 20:19:03 +0200

    Update sidekiq config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,11 +120,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
-    port: ENV['MAILGUN_SMTP_PORT'],
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: ENV['MAILGUN_DOMAIN'],
-    authentication: :plain,
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: 'heroku.com',
+      enable_starttls_auto: true
   }
 end
