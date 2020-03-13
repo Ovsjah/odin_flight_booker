ae789e6ed0cb8ebe42cb87b52d96c863cef1d450
Ovsjah <ovsjah@gmail.com>
2020-03-13 18:40:27 +0200

    Change production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,12 +120,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      authentication: :plain,
-      user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_PASSWORD'],
-      domain: 'heroku.com',
-      enable_starttls_auto: true
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'ovsjazz-odin-flight-booker.heroku.com',
+    authentication: :plain,
   }
 end
