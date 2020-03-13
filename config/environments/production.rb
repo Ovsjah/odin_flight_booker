4c6996864a5fcae947b0db497c6462b12aeccd1b
Ovsjah <ovsjah@gmail.com>
2020-03-13 18:47:59 +0200

    Change production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'ovsjazz-odin-flight-booker.com',
     authentication: :plain,
   }
 end
